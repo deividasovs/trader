ccecea6631fc7292e5fad7d79c3b39e218e7c0cd
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-11-20 20:05:57 +0000

    Trader implementation

--- tradercli.py
+++ tradercli.py
@@ -1,5 +1,3 @@
-# Implement simple trader to let loose and update its strategies
-
 import os
 from dotenv import load_dotenv
 
@@ -11,29 +9,42 @@ from alpaca.trading.enums import AssetClass
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
+from alpaca.data.requests import StockLatestQuoteRequest
+from alpaca.data.historical import StockHistoricalDataClient
+
+
 load_dotenv()
 KEY = os.getenv('KEY')
 SECRET = os.getenv('SECRET')
 
 trading_client = TradingClient(KEY, SECRET, paper=True)
-
+client = StockHistoricalDataClient(KEY, SECRET)
 
 account = trading_client.get_account()
 
 search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
-
 assets = trading_client.get_all_assets(search_params)
 
-def buy_stock_at_market(ticker):
-    
-    qty_to_buy = 1
+def get_latest_stock_price(symbol):
+    multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=[symbol])
 
-    # TODO: get price of ticker and set quantity to that which equals MAX_PRICE_PER_TRADE
+    latest_multisymbol_quotes = client.get_stock_latest_quote(multisymbol_request_params)
+
+    latest_ask_price = latest_multisymbol_quotes[symbol].ask_price
     
+    return latest_ask_price
+
+
+def get_quantity_to_buy_using_max_price(symbol):
+    return MAX_PRICE_PER_TRADE / get_latest_stock_price(symbol)
+
+
+def buy_stock_at_market(symbol):    
+    qty_to_buy = get_quantity_to_buy_using_max_price(symbol)
 
     market_order_data = MarketOrderRequest(
-                    symbol=ticker,
-                    qty=0.023,
+                    symbol=symbol,
+                    qty=qty_to_buy,
                     side=OrderSide.BUY,
                     time_in_force=TimeInForce.DAY
                     )
@@ -42,9 +53,27 @@ def buy_stock_at_market(ticker):
                     order_data=market_order_data
                 )
 
+    print("Buy submitted")
+    print(market_order)
 
-def sell_stock(ticker):
 
+def sell_all_stock_qty(symbol):
+    positions = get_all_positions()
+    qty_to_sell = next((position.qty for position in positions if position.symbol == symbol), None)
+    
+    market_order_data = MarketOrderRequest(
+                    symbol=symbol,
+                    qty=qty_to_sell,
+                    side=OrderSide.SELL,
+                    time_in_force=TimeInForce.DAY
+                    )
+    
+    market_order = trading_client.submit_order(
+                    order_data=market_order_data
+                )
+
+    print("Sale submitted")
+    print(market_order)
 
 
 def get_all_positions():
